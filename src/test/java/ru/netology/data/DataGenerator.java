91f97e4964a648a92ad2476c0a0f745a56c980b3
“dmitry089” <dmitry.karaulov@rambler.ru>
2023-04-20 14:38:14 +0400

    add file

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,71 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.AllArgsConstructor;
+import lombok.Value;
+
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+        // сам запрос
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 OK
+    }
+
+    public static String getRandomLogin() {
+        return faker.name().username();
+    }
+
+    public static String getRandomPassword() {
+        return faker.internet().password();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            var registeredUser = getUser(status);
+            sendRequest(registeredUser);
+            return  registeredUser;
+        }
+    }
+
+    @Value
+    @AllArgsConstructor
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
---
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -0,0 +1,27 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.data.DataGenerator.Registration.getRegisteredUser;
+
+
+public class AuthTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessFullyLoginIfActiveRegisteredUser() {
+        var reqisteredUser =getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(reqisteredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(reqisteredUser.getLogin());
+        $("[data-test-id='action-login']input").click();
+        $(".notification__content").should(Condition.exactText("Неверно указан логин или пароль"));
+
+    }
+}
